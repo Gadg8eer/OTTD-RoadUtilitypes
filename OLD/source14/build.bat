0f38ce7ee1843bfa7181c70ab7cc8144d5bfad27
Gadg8eer <kidpunk@gadg8eer.top>
2024-01-09 22:47:55 -0700

    >_<

--- OLD/source14/build.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-@RD /S /Q ".\.nmlcache"
-@RD /S /Q ".\backups"
-@DEL /S /Q ".\urs.grf"
-@DEL /S /Q "urs.nml"
-:start
-python nml_patcher.py -f "urs.pnml" -o "urs.nml" -b 1 -v 1
-python C:\TTDTools\NML\nmlc C:\TTDTools\GRFs\Utility_Roadtype_Set\OLD\source\urs.nml
-PAUSE
